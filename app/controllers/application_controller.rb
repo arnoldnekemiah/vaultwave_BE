a2305b395fa6940116963c6719988d9ccc924b43
arnoldnekemiah <arnoldnek@gmail.com>
2025-09-13 07:53:09 +0300

    added cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,4 @@ class ApplicationController < ActionController::Base
   
   # Skip CSRF protection for API endpoints
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
-  
-  # CORS headers for API requests
-  before_action :set_cors_headers, if: -> { request.format.json? }
-  
-  private
-  
-  def set_cors_headers
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
 end
